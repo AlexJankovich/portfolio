6bc125aee8fcab55c0b707da3abf36eba8ed8ae7
Alexandr <mylo-box-27@yandex.ru>
2020-10-09 15:22:29 +0300

    second branch

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import style from './Contacts.module.css'
+import React, {FormEvent} from 'react';
+import style from './Contacts.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
+import axios from 'axios'
 
 
 // type ContactsType={
@@ -9,18 +10,43 @@ import styleContainer from '../common/styles/Container.module.css'
 // }
 
 function Contacts() {
+    type FormType = {
+        contacts: string
+        yourName: string
+        message:string
+    }
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const data = {
+            email: e.currentTarget.contacts.value,
+            name: e.currentTarget.yourName.value,
+            message: e.currentTarget.message.value
+        }
+        console.log(data)
+
+        axios.post('http://localhost:3009/sendMessage', data)
+            .then(() => {
+                alert('sent')
+            })
+
+    }
+
     return (
         <div className={style.contactWrapper}>
             <div className={styleContainer.container}>
                 <div className={style.form}>
                     <div className={style.mainDescription}>Контакты</div>
-                    <form action="">
-                        <><input type="text" placeholder='input'/></>
-                        <><input type="text" placeholder='input'/></>
-                        <><textarea>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut autem facere ipsa mollitia officiis quaerat quis sint, tempora tenetur? Inventore perferendis quam repudiandae sapiente sunt. Eum molestiae porro tempora.
-                    </textarea></>
+                    <form action=" " onSubmit={onSubmit}>
+                        <input type="text" placeholder='your email' name={'contacts'}/>
+                        <input type="text" placeholder='your name' name={'yourName'}/>
+                        <textarea name={'message'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut autem facere ipsa mollitia officiis quaerat quis sint, tempora tenetur? Inventore perferendis quam repudiandae sapiente sunt. Eum molestiae porro tempora.
+                    </textarea>
+                        <div>
+                            <button>отправить</button>
+                        </div>
                     </form>
-                    <button>отправить</button>
                 </div>
             </div>
         </div>
